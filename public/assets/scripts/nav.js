a05a77eeed7805d4e7bce7b146d4415ca519b5c8
Qiao Lin <qiaoclin@gmail.com>
2018-10-22 16:10:08 +0200

    init commit

---
+++ public/assets/scripts/nav.js
@@ -0,0 +1,54 @@
+$(document).ready( function(){
+  var link = location.hash
+  var subTabs = ['#home', '#destinations', '#vacation-packages', '#deals&offers']
+
+  setTimeout(function(){
+    if(subTabs.indexOf(link) > -1){
+      $(`[data-value=${link.substring(1)}]`).eq(0)[0].click();
+    }else{
+      setTimeout(openTab, 1000);
+    }
+}, 1000);
+
+// Logo transition
+if($(window).width() < 769){
+    $("h1.flat-link.logo a").animate({'opacity': 0}, 1000,function() {
+      $(this).text("GCATG");
+  }).animate({'opacity': 1}, 30);
+}
+
+  $('.tablink').click(function(){
+    $('.tabcontent').hide();
+    $('.tab-container').removeClass('open');
+    $(this).parent('.tab-container').addClass('open');
+
+    var pageName = this.dataset.value
+    var leavePage = window.location.hash.substring(1)
+    if(leavePage.length == 0){
+        leavePage = 'home'
+    }
+    $('body').removeClass(`${leavePage}Page`);
+    $('body').addClass(`${pageName}Page`);
+
+    $(`#${pageName}-page`).show();
+
+    if($(window).width() < 769){
+      $(window).scrollTop(0);
+    }
+  });
+
+  $('body').on('click', '.open-popup', function () {
+      toggleNav();
+    });
+
+  $('#stickySide a').click(function(){
+    $('.highlight').removeClass('highlight')
+    $(this).addClass('highlight')
+  });
+
+});
+
+
+function openTab(){
+    $("#defaultOpen").click();
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var Q = require('q');
+var pug = require('pug');
+var fs = require('fs');
+var app = express();
+
+var mailer = require('./lib/mailer');
+
+app.use(cors());
+app.use(bodyParser.json({ extended: false }));
+app.use(express.static(__dirname + "/public/assets"));
+
+app.set('views', './public/views');
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res){
+	var imgFiles = getFiles('./public/assets/images');
+	res.render('index', {imgFiles});
+});
+
+var router = express.Router();
+app.use('/sayHello', router);
+router.post('/', mailer.sayHello);
+
+app.listen(3650, () => console.log('listening on 3000 '))
+
+
+function getFiles (dir, files_){
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, files_);
+        } else if(!files[i].startsWith('.')) {
+						var cleanName = name.replace('./public/assets/', ' ')
+            files_.push(cleanName);
+        }
+    }
+    return files_;
+}
